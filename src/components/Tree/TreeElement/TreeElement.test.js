674f63ff413d5f1f7d2873b0cc6573a95f5d0a1f
sdrissi <souleimane.drissielkamili@gmail.com>
2018-07-10 23:27:54 +0200

    Filled test scenarios

--- src/components/Tree/TreeElement/TreeElement.test.js
+++ src/components/Tree/TreeElement/TreeElement.test.js
@@ -12,16 +12,49 @@ it("renders one ul with class Tree__element", () => {
     expect(wrapper.find("ul.Tree__element").length).toEqual(1);
 });
 
-it("renders the items as TreeElements when li is clicked", () => {
-
+it("renders the items as TreeElements when li with class Tree__name is clicked", () => {
+    const props = {
+        items: [
+            {
+                id: 1,
+                name: "item1"
+            }
+        ]
+    };
+    const wrapper = shallow(<TreeElement {...props}/>);
+    wrapper.find("li.Tree__name").simulate("click");
+    expect(wrapper.state().isExpanded).toBe(true);
+    expect(wrapper.find("ul.Tree__element > TreeElement").exists()).toBe(true);
+    expect(wrapper.find("ul.Tree__element > TreeElement").length).toEqual(1);
 });
 
 it("renders the items as TreeElements when isExpanded prop is true", () => {
-
+    const props = {
+        isExpanded: true,
+        items: [
+            {
+                id: 1,
+                name: "item1"
+            }
+        ]
+    };
+    const wrapper = shallow(<TreeElement {...props}/>);
+    expect(wrapper.state().isExpanded).toBe(false);
+    expect(wrapper.find("ul.Tree__element > TreeElement").exists()).toBe(true);
+    expect(wrapper.find("ul.Tree__element > TreeElement").length).toEqual(1);
 });
 
 it("doesn't render the items when li is not clicked and isExpanded prop is false", () => {
-
+    const props = {
+        items: [
+            {
+                id: 1,
+                name: "item1"
+            }
+        ]
+    };
+    const wrapper = shallow(<TreeElement {...props}/>);
+    expect(wrapper.find("ul.Tree__element > TreeElement").exists()).toBe(false);
 });
 
 it("renders name prop in a li element with class Tree__name when highlight prop is not provided", () => {
