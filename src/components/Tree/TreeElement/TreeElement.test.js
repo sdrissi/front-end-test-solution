435cf1a33293296d6049620f4a3f97d8c235b27e
sdrissi <souleimane.drissielkamili@gmail.com>
2018-07-10 23:10:59 +0200

    Wrote tests scenarios for TreeElement

--- src/components/Tree/TreeElement/TreeElement.test.js
+++ src/components/Tree/TreeElement/TreeElement.test.js
@@ -2,12 +2,34 @@ import React from "react";
 import { shallow } from "enzyme";
 import TreeElement from "./TreeElement";
 
-it("should render without crashing", () => {
+it("renders without crashing", () => {
     shallow(<TreeElement/>);
 });
 
-it("should display one unordered list with class Tree__element", () => {
+it("renders one ul with class Tree__element", () => {
     const wrapper = shallow(<TreeElement/>);
     expect(wrapper.find("ul").hasClass("Tree__element")).toBe(true);
     expect(wrapper.find("ul.Tree__element").length).toEqual(1);
 });
+
+it("renders the items as TreeElements when li is clicked", () => {
+
+});
+
+it("renders the items as TreeElements when isExpanded prop is true", () => {
+
+});
+
+it("doesn't render the items when li is not clicked and isExpanded prop is false", () => {
+
+});
+
+it("renders name prop in a li element with class Tree__name when highlight prop is not provided", () => {
+
+});
+
+it("renders name prop in a span when highlight prop is provided and renders highlight prop in a span with class Tree__name--highlighted", () => {
+
+});
+
+
