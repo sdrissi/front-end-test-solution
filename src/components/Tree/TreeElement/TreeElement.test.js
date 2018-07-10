49cc1005955139d9d95da7df7f142bf048b9bddc
sdrissi <souleimane.drissielkamili@gmail.com>
2018-07-10 16:15:51 +0200

    Added one test for TreeElement

--- src/components/Tree/TreeElement/TreeElement.test.js
+++ src/components/Tree/TreeElement/TreeElement.test.js
@@ -5,3 +5,9 @@ import TreeElement from "./TreeElement";
 it("should render without crashing", () => {
     shallow(<TreeElement/>);
 });
+
+it("should display one unordered list with class Tree__element", () => {
+    const wrapper = shallow(<TreeElement/>);
+    expect(wrapper.find("ul").hasClass("Tree__element")).toBe(true);
+    expect(wrapper.find("ul.Tree__element").length).toEqual(1);
+});
