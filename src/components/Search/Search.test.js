f07373345c31177506b14c11f7866beece961bee
sdrissi <souleimane.drissielkamili@gmail.com>
2018-07-16 18:32:30 +0200

    Fixed search failing tests and added one test for testing initial highlight state

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -6,11 +6,11 @@ it("renders without crashing", () => {
     shallow(<Search onChange={jest.fn()}/>);
 });
 
-it("renders one input with class Search", () => {
+it("renders one div with class Search", () => {
     const wrapper = shallow(<Search onChange={jest.fn()}/>);
 
-    expect(wrapper.find("input").hasClass("Search")).toBe(true);
-    expect(wrapper.find("input").length).toEqual(1);
+    expect(wrapper.find("div").hasClass("Search")).toBe(true);
+    expect(wrapper.find("div").length).toEqual(1);
 });
 
 it("renders an input of type text", () => {
@@ -27,6 +27,11 @@ it("renders placeholder according to placeholder prop", () => {
     expect(wrapper.find("input").prop("placeholder")).toEqual("Search");
 });
 
+it("should have highlight state equals false when initialized", () => {
+    const wrapper = shallow(<Search onChange={jest.fn()}/>);
+    expect(wrapper.state().highlight).toBe(false);
+})
+
 it("should respond to change event by calling onChange with input value", () => {
     const onChange = jest.fn();
     const wrapper = shallow(<Search onChange={onChange}/>);
@@ -34,7 +39,7 @@ it("should respond to change event by calling onChange with input value", () => 
     wrapper.find("input").simulate("change", { target: { value: "a" } });
 
     expect(onChange.mock.calls.length).toEqual(1);
-    expect(onChange).toHaveBeenCalledWith("a");
+    expect(onChange).toHaveBeenCalledWith("a", false);
 });
 
 it("should respond to change event by calling onChange prop with input value after the specified timeout", () => {
@@ -49,5 +54,5 @@ it("should respond to change event by calling onChange prop with input value aft
     wrapper.find("input").simulate("change", { target: { value: "a" } });
 
     expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(onChange, 500, "a");
+    expect(setTimeout).toHaveBeenLastCalledWith(onChange, 500, "a", false);
 });
