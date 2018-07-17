8d774293b16a439d4af78c385949256c61b4316b
sdrissi <souleimane.drissielkamili@gmail.com>
2018-07-17 11:17:08 +0200

    Wrote tests scenarios for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
 import { shallow } from "enzyme";
-import App from './App';
+import App from "./App";
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
     shallow(<App/>);
 });
+
+it("renders one div with class App", () => {
+    const wrapper = shallow(<App/>);
+
+    expect(wrapper.find("div").hasClass("App")).toBe(true);
+    expect(wrapper.find("div.App").length).toEqual(1);
+});
+
+it("renders a Search component on top", () => {
+
+});
+
+it("renders a Tree component at the end", () => {
+
+});
+
+it("renders an hr separating Search and Tree components", () => {
+
+});
+
+it("initialized with searchResults state as null", () => {
+
+});
+
+it("handles search with searchTerm non empty by calling search function", () => {
+
+});
+
+it("handles search with search term empty by setting searchResults state to null", () => {
+
+});
+
+it("searches searchTerm through items correctly")
